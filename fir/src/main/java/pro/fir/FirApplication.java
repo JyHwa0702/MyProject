14efb16fd2e4596808d65501d313023f682622fd
JyHwa0702 <livm211@daum.net>
2022-11-14 17:53:05 +0900

    첫 프로젝트 회원가입 11/14

---
+++ fir/src/main/java/pro/fir/FirApplication.java
@@ -0,0 +1,13 @@
+package pro.fir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirApplication.class, args);
+	}
+
+}
