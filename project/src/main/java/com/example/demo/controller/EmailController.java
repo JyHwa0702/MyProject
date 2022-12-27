60a78ad913a9610859f3f628c124dce4f2f26c75
JyHwa0702 <livm211@daum.net>
2022-12-27 12:34:52 +0900

    이메일 코드보내고 받는 중

--- project/src/main/java/com/example/demo/controller/EmailController.java
+++ project/src/main/java/com/example/demo/controller/EmailController.java
@@ -34,9 +34,11 @@ public class EmailController {
         if (byEmail.isPresent()) {
             emailService.sendSimpleMessage(emailDto);
             model.addAttribute("Message","인증코드를 해당 메일로 보냈습니다.");
+            model.addAttribute("codeConfirm",true);
             return "/user/findPwd";
         } else if (byEmail.isEmpty()) {
             model.addAttribute("Message","해당 이메일 유저가 존재하지 않습니다.");
+            model.addAttribute("codeConfirm",false);
             return "/user/findPwd";
         }
         return "/";
