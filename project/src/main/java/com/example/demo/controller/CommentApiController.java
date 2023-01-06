e0fff45f7ab8fadec75385379395308c39553dac
JyHwa0702 <livm211@daum.net>
2023-01-06 13:59:20 +0900

    댓글 구현중 api 쓰기위해 ajax 확인해보자

--- project/src/main/java/com/example/demo/controller/CommentApiController.java
+++ project/src/main/java/com/example/demo/controller/CommentApiController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.config.auth.PrincipalDetails;
+import com.example.demo.dto.BoardDto;
 import com.example.demo.dto.CommentDto;
 import com.example.demo.entity.User;
 import com.example.demo.service.CommentService;
@@ -11,17 +12,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RestController
-@RequestMapping("/api")
+@Controller
+//@RequestMapping("/api")
 public class CommentApiController {
     private final CommentService commentService;
 
     /* CREATE */
     @PostMapping("/board/{id}/comments")
-    public ResponseEntity commentSave(@PathVariable Long id, @RequestBody CommentDto commentDto,
-                                      Authentication authentication){
+    public String commentSave(@PathVariable Long id, CommentDto commentDto,
+                              Authentication authentication){
         PrincipalDetails principal = (PrincipalDetails) authentication.getPrincipal();
         User nowUser = principal.getUser();
-        return ResponseEntity.ok(commentService.commentSave(nowUser.getEmail(),id,commentDto));
+//        return ResponseEntity.ok(commentService.commentSave(nowUser.getEmail(),id,commentDto));
+        Long aLong = commentService.commentSave(nowUser.getEmail(), id, commentDto);
+        return "board/detail";
     }
 }
