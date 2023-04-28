66415823e895a8ce5bae002f595ce96ddf949a51
JyHwa0702 <livm211@daum.net>
2023-04-28 14:12:13 +0900

    Fixed untracked files

---
+++ fir/src/main/java/pro/fir/domain/members/MemberRole.java
@@ -0,0 +1,5 @@
+package pro.fir.domain.members;
+
+public enum MemberRole {
+    USER,ADMIN
+}
---
+++ pro/src/main/java/pro/pro/controller/ApiController.java
@@ -0,0 +1,16 @@
+package pro.pro.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @GetMapping("/name")
+    public String name(){
+        return "내이름";
+    }
+}
